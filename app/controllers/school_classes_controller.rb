1e2f9847d5922f49c9dbe31ec8193a0689cd4a90
Rafael Batista <rafaelb24@yahoo.com>
2019-03-23 05:37:49 +0000

    Automatically backed up by Learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,17 @@
+class SchoolClassesController < ApplicationController 
+  def show 
+  end 
+  
+  def new 
+    @school_class = SchoolClass.new
+  end 
+  
+  def create
+  end 
+  
+  def edit 
+  end 
+  
+  def update 
+  end 
+end
